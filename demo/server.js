406168459ffde704b57bde816c419275636f5b22
Vlad A. Barbu <vlad.a.brb@gmail.com>
2024-06-28 21:01:55 +0300

    fixed demo

--- demo/server.js
+++ demo/server.js
@@ -2,7 +2,7 @@ import express from "express"
 
 const app = express()
 
-app.use(express.static("../pub"))
+app.use(express.static(path.resolve(import.meta.dirname, "../pub")))
 
 const port = 3000
 
